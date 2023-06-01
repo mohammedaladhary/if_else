9e96fbe6173b3f56afd2561cc28ce9790ebc11f2
mohammedaladhary <caladhary@gmail.com>
2023-06-01 12:04:35 +0400

    if else

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("enter the first number:");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter the second number:");
+        int b = int.Parse(Console.ReadLine());
+
+        if (a == b)
+        {
+            Console.WriteLine("EQUAL");
+        }
+        else {
+            Console.WriteLine(" NOT EQUAL ");
+        }
+        
+    }
+}
