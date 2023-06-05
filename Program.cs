3b2a70e727eb9a3d77925bdc21c6d539e1d02b60
mohammedaladhary <caladhary@gmail.com>
2023-06-05 10:07:30 +0400

    multiple conditions

--- Program.cs
+++ Program.cs
@@ -2,19 +2,39 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("enter the first number:");
-        int a = int.Parse(Console.ReadLine());
-        Console.WriteLine("enter the second number:");
-        int b = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Hello, World!");
+        //Console.WriteLine("enter the first number:");
+        //int a = int.Parse(Console.ReadLine());
+        //Console.WriteLine("enter the second number:");
+        //int b = int.Parse(Console.ReadLine());
 
-        if (a == b)
+        //if (a == b)
+        //{
+        //    Console.WriteLine("EQUAL");
+        //}
+        //else {
+        //    Console.WriteLine(" NOT EQUAL ");
+        //}
+
+        int k = 1, l = 1;
+        int r = 5;
+
+       // bool j;
+       // j = k == l;
+      //  Console.WriteLine(j); //true
+
+       // if (k >= l) {
+       if (k < l || r == 5) {
+            Console.WriteLine("condition met.");
+        }
+        //else if (k > l)
+        //{
+        //    Console.WriteLine("else if condition met.");
+        //}
+        else
         {
-            Console.WriteLine("EQUAL");
+            Console.WriteLine("condition is not met.");
         }
-        else {
-            Console.WriteLine(" NOT EQUAL ");
-        }
-        
+
     }
 }
